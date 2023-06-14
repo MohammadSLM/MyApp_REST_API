f32c595a10c487f3ef47fb19019036d54f43637a
Mohammad.slm72 <Mohammad.slm72@gmail.com>
2023-06-14 14:38:11 +0330

    User Entities and configurations

---
+++ Domain/User/User.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.User
+{
+    public class User : BaseEntity
+    {
+        public User()
+        {
+            IsActive = true;
+            FullName = FirstName + " " + LastName;
+        }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        public string? FullName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public bool IsActive { get; set; }
+        public int RoleId { get; set; }
+        public UserRole Role { get; set; }
+
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(p=> p.UserName).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.PasswordHash).IsRequired();
+            builder.Property(p => p.FirstName).HasMaxLength(200);
+            builder.Property(p => p.LastName).HasMaxLength(200);
+            builder.Property(p => p.PhoneNumber).IsRequired();
+            builder.HasOne(p => p.Role).WithMany(p => p.Users).HasForeignKey(p => p.RoleId);
+        }
+    }
+}
---
+++ Domain/User/UserRole.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.User
+{
+    public class UserRole : BaseEntity
+    {
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.Property(p => p.RoleName).IsRequired();
+        }
+    }
+}
