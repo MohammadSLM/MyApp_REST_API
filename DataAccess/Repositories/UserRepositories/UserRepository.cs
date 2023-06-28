3f90e152140dfe2bbadd71ac44f196742976431f
Mohammad.slm72 <Mohammad.slm72@gmail.com>
2023-06-28 15:51:32 +0330

    add repository

---
+++ DataAccess/Repositories/UserRepositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.UserRepositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(ApplicationDbContext dbContext) 
+            : base(dbContext)
+        {
+
+        }
+
+        public Task<User> FindByPhoneNumberAsync(string phonenumber, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(phonenumber))
+            {
+                throw new ArgumentException($"'{nameof(phonenumber)}' نمیتواند خالی باشد.", nameof(phonenumber));
+            }
+
+            return base.Table.Where(a => a.PhoneNumber.Equals(phonenumber, StringComparison.Ordinal)).SingleOrDefaultAsync(cancellationToken);
+        }
+    }
+}
